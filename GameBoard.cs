744562de621aa62eaa9d4a4f4a3e9fca00a5bd9b
AlleXXXeey <131464148+AlleXXXeey@users.noreply.github.com>
2023-08-30 04:02:51 -1100

    Add files via upload

--- GameBoard.cs
+++ GameBoard.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using System.Collections.Generic;
 
 namespace Studio29.TicTacToe
 {
@@ -15,7 +14,9 @@ namespace Studio29.TicTacToe
 
         private char currentPlayer = 'X';
         private bool isGameOver = false;
-        private Dictionary<int, TextMeshProUGUI> cellTexts = new Dictionary<int, TextMeshProUGUI>();
+        private char[] boardState = new char[9];
+
+        private bool isPlayerTurn = true; // Початково гравець починає гру
 
         #endregion
 
@@ -24,11 +25,12 @@ namespace Studio29.TicTacToe
         private void Start()
         {
             InitializeGame();
+            isPlayerTurn = true; // Після ініціалізації гравець починає
         }
 
         #endregion
 
-        #region Logic
+        #region Initialize
 
         private void InitializeGame()
         {
@@ -43,103 +45,219 @@ namespace Studio29.TicTacToe
                 cells[i].onClick.AddListener(() => CellClick(index));
                 TextMeshProUGUI cellText = cells[i].GetComponentInChildren<TextMeshProUGUI>();
                 cellText.text = "";
-                cellTexts.Add(index, cellText);
-                cells[i].interactable = true;
             }
 
+            ResetBoardState();
             isGameOver = false;
         }
+
+        #endregion
+
+        #region Game Logic
 
         private void CellClick(int cellIndex)
         {
             if (!isGameOver && cells[cellIndex].interactable)
             {
-                cells[cellIndex].interactable = false;
-                cellTexts[cellIndex].text = currentPlayer.ToString();
-
-                if (CheckWin())
+                if (currentPlayer == 'X' && isPlayerTurn) // Гравець X ходить лише під час гравцевої черги
                 {
-                    messageText.text = "Гравець " + currentPlayer + " переміг!";
-                    restartButton.gameObject.SetActive(true);
-                    isGameOver = true;
-                }
-                else if (CheckDraw())
-                {
-                    messageText.text = "Нічия!";
-                    restartButton.gameObject.SetActive(true);
-                    isGameOver = true;
-                }
-                else
-                {
-                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-                    messageText.text = "Хід гравця " + currentPlayer;
+                    PlayerMove(cellIndex);
                 }
             }
         }
 
-        private bool CheckWin()
+
+        private void PlayerMove(int cellIndex)
         {
-            // Перевірка по горизонталі, вертикалі та діагоналі
-            for (int i = 0; i < 3; i++)
+            cells[cellIndex].interactable = false;
+            TextMeshProUGUI cellText = cells[cellIndex].GetComponentInChildren<TextMeshProUGUI>();
+            cellText.text = currentPlayer.ToString();
+
+            boardState[cellIndex] = currentPlayer;
+
+            if (CheckWinningMove(currentPlayer, boardState))
             {
-                if (cellTexts[i].text == currentPlayer.ToString() &&
-                    cellTexts[i + 3].text == currentPlayer.ToString() &&
-                    cellTexts[i + 6].text == currentPlayer.ToString())
+                messageText.text = "Гравець " + currentPlayer + " переміг!";
+                restartButton.gameObject.SetActive(true);
+                isGameOver = true;
+            }
+            else if (CheckDraw(boardState))
+            {
+                messageText.text = "Нічия!";
+                restartButton.gameObject.SetActive(true);
+                isGameOver = true;
+            }
+            else
+            {
+                if (isPlayerTurn)
                 {
-                    return true;
-                }
+                    isPlayerTurn = false; // Після ходу гравця, наступний хід буде у бота
+                    currentPlayer = 'O';
+                    messageText.text = "Хід гравця " + currentPlayer;
 
-                if (cellTexts[i * 3].text == currentPlayer.ToString() &&
-                    cellTexts[i * 3 + 1].text == currentPlayer.ToString() &&
-                    cellTexts[i * 3 + 2].text == currentPlayer.ToString())
-                {
-                    return true;
+                    // Виклик функції для ходу бота
+                    BotMove();
                 }
             }
+        }
 
-            if (cellTexts[0].text == currentPlayer.ToString() &&
-                cellTexts[4].text == currentPlayer.ToString() &&
-                cellTexts[8].text == currentPlayer.ToString())
+        private void BotMove()
+        {
+            if (!isPlayerTurn) // Бот (гравець O) ходить, якщо не гравцева черга
             {
-                return true;
+                PlayerMove(FindBestMove());
+                isPlayerTurn = true;
+                currentPlayer = 'X';
+                messageText.text = "Хід гравця " + currentPlayer;                
+            }
+        }
+
+
+        private bool CheckWinningMove(char player, char[] board)
+        {
+            // Перевірка на перемогу гравця
+
+            // Перевірка по горизонталі
+            for (int row = 0; row < 3; row++)
+            {
+                if (board[row * 3] == player && board[row * 3 + 1] == player && board[row * 3 + 2] == player)
+                    return true;
             }
 
-            if (cellTexts[2].text == currentPlayer.ToString() &&
-                cellTexts[4].text == currentPlayer.ToString() &&
-                cellTexts[6].text == currentPlayer.ToString())
+            // Перевірка по вертикалі
+            for (int col = 0; col < 3; col++)
             {
-                return true;
+                if (board[col] == player && board[col + 3] == player && board[col + 6] == player)
+                    return true;
             }
+
+            // Перевірка по діагоналі зліва направо
+            if (board[0] == player && board[4] == player && board[8] == player)
+                return true;
+
+            // Перевірка по діагоналі справа наліво
+            if (board[2] == player && board[4] == player && board[6] == player)
+                return true;
 
             return false;
         }
 
-        private bool CheckDraw()
+        private bool CheckDraw(char[] board)
         {
-            foreach (var cellText in cellTexts.Values)
+            // Перевірка на нічию
+
+            foreach (char cell in board)
             {
-                if (cellText.text == "")
-                {
-                    return false;
-                }
+                if (cell == '\0')
+                    return false; // Якщо є порожні комірки, гра не нічия
             }
-            return true;
+
+            return true; // Якщо всі комірки заповнені, то нічия
         }
 
-        public void RestartGame()
+
+        private void RestartGame()
         {
             currentPlayer = 'X';
             isGameOver = false;
             messageText.text = "Починає гравець X";
             restartButton.gameObject.SetActive(false);
 
-            for (int i = 0; i < cells.Length; i++)
+            ResetBoardState();
+        }
+
+        private void ResetBoardState()
+        {
+            for (int i = 0; i < boardState.Length; i++)
             {
+                boardState[i] = '\0';
                 cells[i].interactable = true;
-                cellTexts[i].text = string.Empty;
+                TextMeshProUGUI cellText = cells[i].GetComponentInChildren<TextMeshProUGUI>();
+                cellText.text = "";
+            }
+        }
+
+        #endregion
+
+        #region MiniMax
+
+        private int FindBestMove()
+        {
+            int bestScore = int.MinValue;
+            int bestMove = -1;
+            int alpha = int.MinValue;
+            int beta = int.MaxValue;
+
+            for (int i = 0; i < boardState.Length; i++)
+            {
+                if (boardState[i] == '\0')
+                {
+                    boardState[i] = 'O';
+                    int moveScore = Minimax(boardState, 0, false, alpha, beta);
+                    boardState[i] = '\0';
+
+                    if (moveScore > bestScore)
+                    {
+                        bestScore = moveScore;
+                        bestMove = i;
+                    }
+
+                    alpha = Mathf.Max(alpha, bestScore);
+                    if (beta <= alpha)
+                        break;
+                }
+            }
+
+            return bestMove;
+        }
+
+        private int Minimax(char[] board, int depth, bool isMaximizing, int alpha, int beta)
+        {
+            if (CheckWinningMove('O', board))
+                return 1; 
+            if (CheckWinningMove('X', board))
+                return -1;
+            if (CheckDraw(board) || depth >= 5)
+                return 0;
+
+            if (isMaximizing)
+            {
+                int bestScore = int.MinValue;
+                for (int i = 0; i < board.Length; i++)
+                {
+                    if (board[i] == '\0')
+                    {
+                        board[i] = 'O'; // Змінено 'X' на 'O'
+                        int score = Minimax(board, depth + 1, false, alpha, beta);
+                        board[i] = '\0';
+                        bestScore = Mathf.Max(bestScore, score);
+                        alpha = Mathf.Max(alpha, bestScore);
+                        if (beta <= alpha)
+                            break;
+                    }
+                }
+                return bestScore;
+            }
+            else
+            {
+                int bestScore = int.MaxValue;
+                for (int i = 0; i < board.Length; i++)
+                {
+                    if (board[i] == '\0')
+                    {
+                        board[i] = 'X'; // Змінено 'O' на 'X'
+                        int score = Minimax(board, depth + 1, true, alpha, beta);
+                        board[i] = '\0';
+                        bestScore = Mathf.Min(bestScore, score);
+                        beta = Mathf.Min(beta, bestScore);
+                        if (beta <= alpha)
+                            break;
+                    }
+                }
+                return bestScore;
             }
         }
 
         #endregion
     }
-}
+} 
